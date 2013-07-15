22b13cd50bec8af73d8f5d6be1f7367e97debe31
William Ngo <wngo87@gmail.com>
2013-07-14 23:04:51 -0700

    Completed _.every

--- src/underbar.js
+++ src/underbar.js
@@ -155,6 +155,12 @@ var _ = { };
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    return _.reduce(collection, function(trueSoFar, item) {
+	    if(!trueSoFar) {
+		    return false;
+	    }
+	    return typeof iterator === 'function' ? iterator(item) == true : item == true;
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
